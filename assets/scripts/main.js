68e77a09c92075285c001069ff93b2545f14f384
Marco Silvestri <m.silvestri03@gmail.com>
2020-05-04 14:21:17 +0200

    Mile 1 - 0.0.1
    Main HTML structure

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    
+    //  Init Handlebars
+    var source = $('#movie-template');
+    var template = Handlebars.compile(source);
+
+    
+
+}); //  END of DOCUMENT READY
