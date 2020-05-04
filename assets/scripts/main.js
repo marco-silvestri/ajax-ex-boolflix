aaa50d25c19a0a42271544b3ebc0e12700c8f4ba
Marco Silvestri <m.silvestri03@gmail.com>
2020-05-04 16:04:14 +0200

    Mile 1 - 0.1.1
    *Default language it-IT;
    *Handled no matching results exception

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     //  Global variables
     var myApi = 'https://api.themoviedb.org/3/search/movie';
     var myToken = 'e80fd63011e84d9eb3ea864a957b8a81';
+    var languageSearch = 'it-IT'; //Default language
 
     //  jQuery refs
     var inputSearch = $('#app__search-area__movie-search');
@@ -13,26 +14,28 @@ $(document).ready(function () {
     var source = $('#movie-template').html();
     var template = Handlebars.compile(source);
 
+    //  Search by hitting ENTER
     inputSearch.keydown(function (e) { 
         switch (e.which){
-            case 13:
-                searchAndPrint(myApi, myToken, template, inputSearch, movieGround);
+            case 13: // 13 is ENTER
+                searchAndPrint(myApi, myToken, template, inputSearch, movieGround, languageSearch);
                 break;
             default:
                 break
         }
     });
 
+    //  Search by clicking on the button
     buttonSearch.click(function () { 
-        searchAndPrint(myApi, myToken, template, inputSearch, movieGround);
+        searchAndPrint(myApi, myToken, template, inputSearch, movieGround, languageSearch);
     });
 }); //  END of DOCUMENT READY
 
-/***************
-*   FUNCTIONS
-****************/
+/****************
+*   FUNCTIONS   *
+*****************/
 
-function searchAndPrint(myApi, myToken, template, input, destination){
+function searchAndPrint(myApi, myToken, template, input, destination, languageSearch){
     destination.html('');
         var querySearch  = input.val();
         $.ajax({
@@ -40,20 +43,28 @@ function searchAndPrint(myApi, myToken, template, input, destination){
             url: myApi,
             data: {
                 api_key : myToken,
+                language : languageSearch,
                 query : querySearch
             },
             success: function (response) {
                 var totalResults = response.results.length;
-                for (var i = 0; i < totalResults; i++){
-                    var thisResult = response.results[i];
-                    var templateData = {
-                        title : thisResult.title,
-                        originalTitle : thisResult.original_title,
-                        originalLanguage : thisResult.original_language,
-                        voteAverage : thisResult.vote_average
-                    };
-                    var output = template(templateData);
-                    destination.append(output);
+                //  If there are no elements matching the search criteria
+                if (response.total_results === 0){
+                    $('#app__movie-ground').html('Nessun film trovato');
+                }
+                //  Matching search elements
+                else {
+                    for (var i = 0; i < totalResults; i++){
+                        var thisResult = response.results[i];
+                        var templateData = {
+                            title : thisResult.title,
+                            originalTitle : thisResult.original_title,
+                            originalLanguage : thisResult.original_language,
+                            voteAverage : thisResult.vote_average
+                        };
+                        var output = template(templateData);
+                        destination.append(output);
+                    }
                 }
             },
             error: function () {  
