bf0463c0edcf5d61fff1fa49692394876716746a
Marco Silvestri <m.silvestri03@gmail.com>
2020-05-05 11:31:47 +0200

    Mile 1 - 0.1.2
    Code refactoring & optimization

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     
     //  Global variables
-    var myApi = 'https://api.themoviedb.org/3/search/movie';
+    var apiMovie = 'https://api.themoviedb.org/3/search/movie';
     var myToken = 'e80fd63011e84d9eb3ea864a957b8a81';
     var languageSearch = 'it-IT'; //Default language
 
@@ -18,7 +18,7 @@ $(document).ready(function () {
     inputSearch.keydown(function (e) { 
         switch (e.which){
             case 13: // 13 is ENTER
-                searchAndPrint(myApi, myToken, template, inputSearch, movieGround, languageSearch);
+                searchAndPrint(apiMovie, myToken, template, inputSearch, movieGround, languageSearch);
                 break;
             default:
                 break
@@ -27,7 +27,7 @@ $(document).ready(function () {
 
     //  Search by clicking on the button
     buttonSearch.click(function () { 
-        searchAndPrint(myApi, myToken, template, inputSearch, movieGround, languageSearch);
+        searchAndPrint(apiMovie, myToken, template, inputSearch, movieGround, languageSearch);
     });
 }); //  END of DOCUMENT READY
 
@@ -36,39 +36,71 @@ $(document).ready(function () {
 *****************/
 
 function searchAndPrint(myApi, myToken, template, input, destination, languageSearch){
-    destination.html('');
+        cleanAll(destination);
         var querySearch  = input.val();
-        $.ajax({
-            type: "GET",
-            url: myApi,
-            data: {
-                api_key : myToken,
-                language : languageSearch,
-                query : querySearch
-            },
-            success: function (response) {
-                var totalResults = response.results.length;
-                //  If there are no elements matching the search criteria
-                if (response.total_results === 0){
-                    $('#app__movie-ground').html('Nessun film trovato');
-                }
-                //  Matching search elements
-                else {
-                    for (var i = 0; i < totalResults; i++){
-                        var thisResult = response.results[i];
-                        var templateData = {
-                            title : thisResult.title,
-                            originalTitle : thisResult.original_title,
-                            originalLanguage : thisResult.original_language,
-                            voteAverage : thisResult.vote_average
-                        };
-                        var output = template(templateData);
-                        destination.append(output);
+        if (querySearch.trim() != ''){
+            $.ajax({
+                type: "GET",
+                url: myApi,
+                data: {
+                    api_key : myToken,
+                    language : languageSearch,
+                    query : querySearch
+                },
+                success: function (response) {
+                    var totalResults = response.results.length;
+                    //  If there are no elements matching the search criteria
+                    if (response.total_results === 0){
+                        $('#app__movie-ground').html('Nessun film trovato');
+                        input.select();
                     }
+                    //  Matching search elements
+                    else {
+                        for (var i = 0; i < totalResults; i++){
+                            printMovieCards(i, response, template, destination, 'movie');
+                        }
+                    }
+                },
+                error: function () {  
+                    console.log('Cannot retrieve the API, try again later');   
                 }
-            },
-            error: function () {  
-                console.log('Cannot retrieve the API, try again later');   
-            }
-        });
+            });
+        }
+        else {
+            $('#app__movie-ground').html('Inserisci un valore nella ricerca');
+            input.val('');
+            input.focus();
+        }
+        
+}
+
+//  Clean a destination
+function cleanAll(destination){
+    destination.html('');
+}
+
+// Arrange the all the element[i] in a template and print to destination
+function printMovieCards(i, response, template, destination, condition){
+        if (condition == 'movie'){
+            var thisResult = response.results[i];
+            var templateData = {
+                title : thisResult.title,
+                originalTitle : thisResult.original_title,
+                originalLanguage : thisResult.original_language,
+                voteAverage : thisResult.vote_average
+            };
+            var output = template(templateData);
+            destination.append(output);
+        }
+        else if (condition == 'series'){
+            var thisResult = response.results[i];
+            var templateData = {
+                title : thisResult.name,
+                originalTitle : thisResult.original_name,
+                originalLanguage : thisResult.original_language,
+                voteAverage : thisResult.vote_average
+            };
+            var output = template(templateData);
+            destination.append(output);
+        }
 }
